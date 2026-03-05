2604a7e921ce837445228418d06c90090e71b1a3
fruittail <villarindirkdannielle@gmail.com>
2026-03-05 21:28:31 +0800

    ahhh

--- User.java
+++ User.java
@@ -1,12 +1,18 @@
-
-abstract class User {
-
+public abstract class User {
     private String id;
     private String name;
 
-    User(String id, String name) {
+    public User(String id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public abstract void displayMenu();
